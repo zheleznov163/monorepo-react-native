4771162004eb1db95d99f9933b18222f337a3d4c
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2020-07-27 22:29:15 +0300

    settings: start up react native

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -5,13 +5,42 @@
  * @format
  */
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
+const path = require('path');
+const blacklist = require('metro').createBlacklist;
+const { getDefaultConfig } = require('metro-config');
+
+// const reactNativePath = require.resolve('react-native');
+// const reactNativeFolder = `${
+//   reactNativePath.split('node_modules/react-native/')[0]
+// }node_modules/react-native/`;
+
+const getConfig = async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false
+        }
+      })
+    },
+
+    watchFolders: [path.resolve(__dirname, '../../')],
+
+    resolver: {
+      // extraNodeModules: {
+      //   'core': path.resolve(__dirname, '../core'),
+      //   'react-native': path.resolve(__dirname, './node_modules/react-native')
+      // },
+      blacklistRE: blacklist([/#current-cloud-backend\/.*/]),
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg']
+    }
+  };
 };
+
+module.exports = getConfig();
